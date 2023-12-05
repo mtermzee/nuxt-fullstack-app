7734926520bd2d720de81ed18a3f287dc76485cb
mh.termzee <m.termzee@basecom.com>
2023-12-05 16:56:41 +0100

    add nuxtLink

--- components/nav/AppHeader.vue
+++ components/nav/AppHeader.vue
@@ -26,11 +26,11 @@
 				</NuxtLink>
 			</nav>
 			<div class="items-center h-full flex gap-2">
-				<a href="#_" class="mr-5 font-medium hover:text-gray-900">Login</a>
-				<a href="#_"
+				<NuxtLink to="/login" class="mr-5 font-medium hover:text-gray-900">Login</NuxtLink>
+				<NuxtLink to="/register"
 					class="px-4 py-2 text-xs font-bold text-white uppercase transition-all duration-150 bg-teal-500 rounded shadow outline-none active:bg-teal-600 hover:shadow-md focus:outline-none ease">
 					Sign Up
-				</a>
+				</NuxtLink>
 				<button
 					class="px-4 py-2 text-xs font-bold text-white uppercase transition-all duration-150 bg-teal-500 rounded shadow outline-none active:bg-teal-600 hover:shadow-md focus:outline-none ease"
 					@click="setColorTheme($colorMode.preference == 'dark' ? 'light' : 'dark')">
