797f9b833a08bd16d51b0336164835ba7c972b8c
mh.termzee <m.termzee@basecom.com>
2023-12-05 10:38:56 +0100

    init

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+	<div style="display: flex; min-height: 100vh; flex-direction: column">
+		<NavAppHeader />
+
+		<main>
+			<slot />
+		</main>
+
+		<div style="flex-grow: 1"></div>
+
+		<NavAppFooter />
+	</div>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="flex flex-col w-4/5 m-auto justify-center items-start">
+		<h1>About page</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+			perferendis ipsa quae non incidunt, officia dolor ducimus hic corporis.
+			Repudiandae.
+		</p>
+		<NuxtLink to="/products">Products</NuxtLink>
+	</div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
