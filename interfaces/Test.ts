11e83550c30ea7d880241165ea71b816252d87c3
mh.termzee <m.termzee@basecom.com>
2023-12-05 10:44:28 +0100

    add test interface

---
+++ interfaces/Test.ts
@@ -0,0 +1,12 @@
+/*export interface Test {
+	id: number;
+	title: string;
+	description: string;
+	image: string;
+	price: number;
+	category: string;
+	rating: {
+		rate: number;
+		count: number;
+	};
+}*/
